46feab839d27bce9d011576bb416da54f3ab5ceb
Alex Wong <alex.wong@clientrelationship.com>
2019-11-09 00:07:14 +0000

    added ForkTSCheckerWebpackPlugin to render error on browser

--- config/storybook/webpack.config.js
+++ config/storybook/webpack.config.js
@@ -1,3 +1,5 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = ({config}) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -8,5 +10,14 @@ module.exports = ({config}) => {
     ]
   })
   config.resolve.extensions.push('.ts', '.tsx')
+
+  config.plugins.push(
+    new ForkTsCheckerWebpackPlugin({
+      tsconfig: './tsconfig.json',
+      async: false,
+      checkSyntacticErrors: true
+    })
+  )
+
   return config
 }
