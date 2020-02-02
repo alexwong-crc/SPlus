2b0175d43ee2dc77607565268d43e57e3ea701cb
Alex Wong <alex.wong@clientrelationship.com>
2020-02-02 08:29:21 +0000

    added a type prop to butotn and updated tests

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -4,7 +4,22 @@ import Button from "./index";
 
 describe("Button", () => {
   it("should snapshot the button", () => {
-    const component = shallow(<Button name="typescript test" />);
+    const component = shallow(<Button name="Submit" />);
     expect(component).toMatchSnapshot();
+  });
+
+  it("should contain text in the button", () => {
+    const component = shallow(<Button name="Click Here" />);
+    expect(component.text()).toEqual("Click Here");
+  });
+
+  it("should default to type button when type is not supplied", () => {
+    const component = shallow(<Button name="No Type" />);
+    expect(component.props().type).toEqual("button");
+  });
+
+  it("should accept the button type value", () => {
+    const component = shallow(<Button name="Submit Type" type="submit" />);
+    expect(component.props().type).toEqual("submit");
   });
 });
--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 
-const StyledButton = styled.button`
+interface IStyledButton {
+  type: "button" | "submit" | "reset";
+}
+
+const StyledButton = styled.button<IStyledButton>`
   padding: 1rem;
 `;
 interface IProps {
   name: string;
+  type?: "button" | "submit" | "reset";
 }
 
-const Button: React.SFC<IProps> = ({ name }: IProps) => (
-  <StyledButton type="button">
-    Hello
-    {name}
-  </StyledButton>
+const Button: React.SFC<IProps> = ({ name, type = "button" }: IProps) => (
+  <StyledButton type={type}>{name}</StyledButton>
 );
 
 export default Button;
