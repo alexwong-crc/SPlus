63ec40517ce7e862bdb76313014630e4a809bac6
Alex Wong <alex.wong@clientrelationship.com>
2020-02-10 19:45:46 +0000

    updated the button test file to reflect latest button changes

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -4,22 +4,22 @@ import Button, { StyledButton } from "./Button";
 
 describe("Button Component", () => {
   it("should snapshot the button", () => {
-    const component = shallow(<Button name="Submit" />);
+    const component = shallow(<Button>Click Here</Button>);
     expect(component).toMatchSnapshot();
   });
 
   it("should contain text in the button", () => {
-    const component = shallow(<Button name="Click Here" />);
+    const component = shallow(<Button>Click Here</Button>);
     expect(component.text()).toEqual("Click Here");
   });
 
   it("should default to type button when type is not supplied", () => {
-    const component = shallow(<Button name="No Type" />);
+    const component = shallow(<Button>Click Here</Button>);
     expect(component.props().type).toEqual("button");
   });
 
   it("should accept the button type value", () => {
-    const component = shallow(<Button name="Submit Type" type="submit" />);
+    const component = shallow(<Button type="submit">Click Here</Button>);
     expect(component.props().type).toEqual("submit");
   });
 });
