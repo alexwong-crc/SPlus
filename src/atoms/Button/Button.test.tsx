c22e2002be59d3fabbfc6d0b160d64b993afec53
Alex Wong <alex.wong@clientrelationship.com>
2020-02-10 21:49:16 +0000

    updated test to take in theme prop

--- src/atoms/Button/Button.test.tsx
+++ src/atoms/Button/Button.test.tsx
@@ -1,32 +1,38 @@
 import { shallow } from "enzyme";
 import React from "react";
+import colourTheme from "../../styles/colourTheme";
 import Button, { StyledButton } from "./Button";
 
+const themeProp = { theme: colourTheme };
 describe("Button Component", () => {
   it("should snapshot the button", () => {
-    const component = shallow(<Button>Click Here</Button>);
+    const component = shallow(<Button {...themeProp}>Click Here</Button>);
     expect(component).toMatchSnapshot();
   });
 
   it("should contain text in the button", () => {
-    const component = shallow(<Button>Click Here</Button>);
+    const component = shallow(<Button {...themeProp}>Click Here</Button>);
     expect(component.text()).toEqual("Click Here");
   });
 
   it("should default to type button when type is not supplied", () => {
-    const component = shallow(<Button>Click Here</Button>);
+    const component = shallow(<Button {...themeProp}>Click Here</Button>);
     expect(component.props().type).toEqual("button");
   });
 
   it("should accept the button type value", () => {
-    const component = shallow(<Button type="submit">Click Here</Button>);
+    const component = shallow(
+      <Button type="submit" {...themeProp}>
+        Click Here
+      </Button>
+    );
     expect(component.props().type).toEqual("submit");
   });
 });
 
 describe("StyledButton Component", () => {
   it("should snapshot the styled button", () => {
-    const component = shallow(<StyledButton type="button" />);
+    const component = shallow(<StyledButton type="button" {...themeProp} />);
     expect(component).toMatchSnapshot();
   });
 });
