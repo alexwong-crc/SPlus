fd32c480de0481aad6ff1b26973ee000d26cc8eb
Alex Wong <alex.wong@clientrelationship.com>
2020-02-11 22:25:33 +0000

    moved styled button to its own file

--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,48 +1,6 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import StatusIcon from "./StatusIcon";
-
-interface IStyledButton {
-  type: "button" | "submit" | "reset";
-  showText?: boolean;
-}
-
-export const StyledButton = styled.button<IStyledButton>`
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  background-color: ${({ theme }): string => theme.action.main.code};
-  color: ${({ theme }): string => theme.action.main.text};
-  border: none;
-  font-size: 0.8rem;
-  position: relative;
-  :active {
-    background-color: ${({ theme }): string => theme.action.dark.code};
-  }
-  :hover {
-    cursor: pointer;
-  }
-  :focus {
-    outline: none;
-  }
-  :disabled {
-    cursor: default;
-    opacity: 0.6;
-    background-color: ${({ theme }): string => theme.action.main.code};
-  }
-  span.text {
-    opacity: ${({ showText }): number => (showText ? 1 : 0)};
-  }
-  span.icon {
-    width: 100%;
-    height: 100%;
-    top: 0;
-    left: 0;
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-`;
+import StyledButton from "./StyledButton";
 
 interface IButton {
   type?: "button" | "submit" | "reset";
