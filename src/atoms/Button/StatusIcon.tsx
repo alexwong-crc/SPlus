08ccaf44b86e1a5437b73e0162d5b82f80c361d1
Alex Wong <alex.wong@clientrelationship.com>
2020-02-16 23:33:01 +0000

    added animation to the success/fail icons in the button

--- src/atoms/Button/StatusIcon.tsx
+++ src/atoms/Button/StatusIcon.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { StatusProps } from "./Button";
 import Icon from "../Icon";
 
@@ -8,25 +9,56 @@ interface IStatusIcon {
 
 type IconNames = "spinner" | "check" | "times";
 
+const StyledIcon = styled.div<IStatusIcon>`
+  position: absolute;
+  transform: translateY(-200%);
+  transition: 0.3s;
+  &.slideInOut {
+    animation-name: slideInOut;
+    animation-duration: 1s;
+  }
+
+  @keyframes slideInOut {
+    0% {
+      transform: translateY(-200%);
+    }
+    25% {
+      transform: translateY(0);
+    }
+    75% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-200%);
+    }
+  }
+`;
+
 const StatusIcon: React.SFC<IStatusIcon> = ({ status }: IStatusIcon) => {
   const renderIcon = (): IconNames => {
     switch (status) {
-      case "loading":
-        return "spinner";
       case "success":
         return "check";
       case "failure":
         return "times";
       default:
-        return "spinner";
+        return "times";
     }
   };
-  return !status ? null : (
-    <Icon
-      icon={renderIcon()}
-      spin={status === "loading" ? true : false}
-      transform="grow-5"
-    />
+
+  return (
+    <>
+      <StyledIcon
+        className={
+          status === "success" || status === "failure" ? "slideInOut" : ""
+        }
+      >
+        <Icon icon={renderIcon()} transform="grow-5" />
+      </StyledIcon>
+      {status === "loading" ? (
+        <Icon icon="spinner" spin transform="grow-5" />
+      ) : null}
+    </>
   );
 };
 
